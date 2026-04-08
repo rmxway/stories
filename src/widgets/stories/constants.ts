9e991a3070685f345d3471d7053a41b868314dbf
rmxway <rmxway@mail.ru>
2026-04-08 11:38:16 +0300

    Update story images and modify constants to reference new image file for story 3

--- src/widgets/stories/constants.ts
+++ src/widgets/stories/constants.ts
@@ -5,7 +5,7 @@ export const STORY_DURATION_SEC = 5;
 export const STORIES = [
 	{ id: '1', src: '/img/stories/1.jpg' },
 	{ id: '2', src: '/img/stories/2.jpg' },
-	{ id: '3', src: '/img/stories/1.jpg' },
+	{ id: '3', src: '/img/stories/3.jpg' },
 ] as const;
 
 export type StoryItem = (typeof STORIES)[number];
